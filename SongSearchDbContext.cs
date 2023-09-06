5f8b9608397bf6ce90bb83775683ce157a2ddac4
Jasper Baltz <nickolas.baltz@gmail.com>
2023-09-05 19:35:39 -0500

    deleted files?

---
+++ SongSearchDbContext.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SongSearch.Models;
+using System.Runtime.CompilerServices;
+
+public class SongSearchDbContext : DbContext
+{
+
+    public DbSet<Artist> Artists { get; set; }
+    public DbSet<Song> Songs { get; set; }
+    public DbSet<Song_Genre> Song_Genres { get; set; }
+    public DbSet<Genre> Genres { get; set; }
+
+    public SongSearchDbContext(DbContextOptions<SongSearchDbContext> context) : base(context)
+    {
+
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Artist>().HasData(new Artist[]
+        {
+            new Artist { Id = , Name = , Age = , Bio = , Songs = },
+            new Artist { Id = , Name = , Age = , Bio = , Songs = },
+            new Artist { Id = , Name = , Age = , Bio = , Songs = },
+            new Artist { Id = , Name = , Age = , Bio = , Songs = },
+            new Artist { Id = , Name = , Age = , Bio = , Songs = }
+        });
+        modelBuilder.Entity<Song>().HasData(new Song[]
+        {
+            new Song { Id = , ArtistId = , Title = , Album = , Length = },
+            new Song { Id = , ArtistId = , Title = , Album = , Length = },
+            new Song { Id = , ArtistId = , Title = , Album = , Length = },
+            new Song { Id = , ArtistId = , Title = , Album = , Length = },
+            new Song { Id = , ArtistId = , Title = , Album = , Length = },
+            new Song { Id = , ArtistId = , Title = , Album = , Length = },
+            new Song { Id = , ArtistId = , Title = , Album = , Length = },
+            new Song { Id = , ArtistId = , Title = , Album = , Length = },
+            new Song { Id = , ArtistId = , Title = , Album = , Length = },
+            new Song { Id = , ArtistId = , Title = , Album = , Length = },
+            new Song { Id = , ArtistId = , Title = , Album = , Length = }
+        });
+    
+        modelBuilder.Entity<Song_Genre>().HasData(new Song_Genre[]
+        {
+            new Song_Genre { Id = , GenreId = , SongId = },
+            new Song_Genre { Id = , GenreId = , SongId = },
+            new Song_Genre { Id = , GenreId = , SongId = },
+            new Song_Genre { Id = , GenreId = , SongId = },
+            new Song_Genre { Id = , GenreId = , SongId = },
+            new Song_Genre { Id = , GenreId = , SongId = },
+            new Song_Genre { Id = , GenreId = , SongId = },
+            new Song_Genre { Id = , GenreId = , SongId = },
+            new Song_Genre { Id = , GenreId = , SongId = },
+            new Song_Genre { Id = , GenreId = , SongId = }
+        });
+    
+        modelBuilder.Entity<Genre>().HasData(new Genre []
+        {
+            new Genre { Id = , Description = },
+            new Genre { Id = , Description = },
+            new Genre { Id = , Description = },
+            new Genre { Id = , Description = },
+            new Genre { Id = , Description = }
+        });
+    }
+   
+}
+
