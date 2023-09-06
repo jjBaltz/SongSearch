35fa9dd4eea2d725b936169cbf553882b3ef05f6
Jasper Baltz <nickolas.baltz@gmail.com>
2023-09-05 19:04:47 -0500

    created model classes

---
+++ models/Artists.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace SongSearch.Models;
+
+public class Artist
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string Bio { get; set; }
+    public List<Song> Songs { get; set; }
+}
---
+++ models/Song_Genres.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace SongSearch.Models;
+
+public class Song_Genre
+{
+    public int Id { get; set; }
+    public int SongId { get; set; }
+    public Song Song { get; set; }
+    public int GenreId { get; set; }
+}
