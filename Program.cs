154fcdfb8266a72dcbe116119b39d0bcb6e08536
Jasper Baltz <nickolas.baltz@gmail.com>
2023-09-08 19:09:49 -0500

    all Artist api calls done

--- Program.cs
+++ Program.cs
@@ -27,8 +27,58 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+//ARTIST ENDPOINTS
+app.MapPost("/api/artists", (SongSearchDbContext db, Artist artist) =>
+{
+    try
+    {
+    db.Artists.Add(artist);
+    db.SaveChanges();
+    return Results.Created($"/api/artists/{artist.ArtistId}", artist);
+    }
+    catch (DbUpdateException)
+    {
+        return Results.NotFound();
+    }
+});
 
+app.MapDelete("/api/artists/{id}", (SongSearchDbContext db, int id) =>
+{
+    Artist artist = db.Artists.SingleOrDefault(artist => artist.ArtistId == id);
+    if (artist == null)
+    {
+        return Results.NotFound();
+    }
+    db.Artists.Remove(artist);
+    db.SaveChanges();
+    return Results.NoContent();
+});
 
+app.MapPut("/api/artists/{id}", (SongSearchDbContext db, int id, Artist artist) =>
+{
+    Artist artistToUpdate = db.Artists.SingleOrDefault(artist => artist.ArtistId == id);
+    if (artistToUpdate == null)
+    {
+        return Results.NotFound();
+    }
+    artistToUpdate.Name = artist.Name;
+    artistToUpdate.Age = artist.Age;
+    artistToUpdate.Bio = artist.Bio;
+
+    db.Update(artistToUpdate);
+    db.SaveChanges();
+    return Results.Ok(artistToUpdate);
+});
+
+app.MapGet("/api/artists", (SongSearchDbContext db) =>
+{
+    return db.Artists.ToList();
+});
+
+app.MapGet("/api/artists/{id}", (SongSearchDbContext db, int id) =>
+{
+    return db.Artists.Single(a => a.ArtistId == id);
+});
 
 
 app.Run();
